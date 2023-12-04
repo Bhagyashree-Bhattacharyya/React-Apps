9af86c86a6d7a52f8b80a09dbba7d9e874d2e152
Bhagyashree <bbhattacharyya135@gmail.com>
2023-12-04 21:55:16 +0530

    use of states

--- src/components/Products/ListItems/ListItem.js
+++ src/components/Products/ListItems/ListItem.js
@@ -1,6 +1,32 @@
 import AddToCardIcon from "../../../assets/icons/add_cart.svg"
+import {useState} from "react"
 
 const ListItem = ({data}) => {
+/*
+    const [message, setMessage] = useState("Not added to the cart yet!!")
+
+//    let message = "Not added to the cart yet!!"
+    const handleClick = () => {
+//        message = "Added to the cart!"
+        setMessage("Added to the cart!")
+        console.log("clicked!!", message)
+    }
+*/
+    
+    const [counter, setCounter] = useState(0)
+
+    const increaseCounterByOne = () => {
+        //increament logic
+        setCounter(counter + 1);
+    }
+
+    const descreaseCounterByOne = () => {
+        //decreament logic
+        if(counter <= 0){
+            return;
+        }
+        setCounter(counter - 1);
+    }
 
     return (
         <div className={"item-card"}>
@@ -16,10 +42,20 @@ const ListItem = ({data}) => {
                     <h4>{data.title}</h4>
                 </div>
             </div>
+            <div>
+            {/*<small className={"cart-msg"}>{message}</small>*/}
             <button className={"cart-add"}>
                 <span>Add to Cart</span>
                 <img src={AddToCardIcon} alt="cart icon" style={{ width: '20px', height: '20px' }}/>
             </button>
+            <span>
+                <div className={"cart-addon"}>
+                    <button onClick={descreaseCounterByOne}><span>-</span></button>
+                    <span className={"counter"}>{counter}</span>
+                    <button onClick={increaseCounterByOne}><span>+</span></button>
+                </div>
+            </span>
+            </div>
         </div>
     )
 }
